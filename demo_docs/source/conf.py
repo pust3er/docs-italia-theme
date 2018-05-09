ec010c65348384c052a79b16eb18b9d207114e6d
Davide Porrovecchio <pdavide@gmail.com>
2018-05-09 18:10:27 +0200

    Include hidden toc

--- demo_docs/source/conf.py
+++ demo_docs/source/conf.py
@@ -125,6 +125,9 @@ if on_rtd:
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'docs-italia-theme'
+html_theme_options = {
+    "includehidden": "true"
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
