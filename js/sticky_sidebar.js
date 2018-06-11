6c675e4cd6b054a10c8b8fa847ddefeaddb6a371
Luca Morelli <luca.morelli@wellnet.it>
2018-06-12 00:18:27 +0200

    sticky sidebar soluzione 2 jQuery full custom

--- js/sticky_sidebar.js
+++ js/sticky_sidebar.js
@@ -1,6 +1,24 @@
 // Module dependencies
 var StickySidebar = require('sticky-sidebar');
 
+// This Function will always return the initial font-size of the html element
+var rem = function rem() {
+  var html = document.getElementsByTagName('html')[0];
+
+  return function () {
+    return parseInt(window.getComputedStyle(html)['fontSize']);
+  }
+}();
+
+// This function will convert pixel to rem
+function toRem(length) {
+  return (parseInt(length) / rem());
+}
+
+function toPix(length) {
+  return (parseInt(length) * rem());
+}
+
 // Sticky sidebar
 module.exports = themeStickySidebar = (function ($) {
 
@@ -9,45 +27,90 @@ module.exports = themeStickySidebar = (function ($) {
 
       $(document).ready(function () {
 
-        var sidebar = document.getElementsByClassName('docs-italia-menu')[0],
-          stickySidebar = new StickySidebar(sidebar, {
-           containerSelector: '#main',
-           innerWrapperSelector: '#desktop-menu',
-           topSpacing: 85,
-           bottomSpacing: 20
-         }),
-         sidebar__inner = document.getElementById('desktop-menu');
+        $(window).on('scroll', function(event) {
+            var scrollPosition = $(window).scrollTop(),
+              scroll_offset = $('header').outerHeight(),
+              sidebar = $('#desktop-menu'),
+              sidebar_container_height = sidebar.closest('.docs-italia-menu').outerHeight(),
+              sidebar_main_container = sidebar.closest('#main'),
+              window_height = $(window).outerHeight();
 
-        // sidebar.addEventListener('affix.container-bottom.stickySidebar', function() {
-        //   console.log('"affix.container-bottom.stickySidebar" triggered');
-        //
-        // });
-        // sidebar.addEventListener('affixed.container-bottom.stickySidebar', function() {
-        //
-        //   console.log('2 translate js=', sidebar__inner.style.transform);
-        //
-        //   var heights_array = sidebar__inner.style.transform.slice(sidebar__inner.style.transform.indexOf('('), sidebar__inner.style.transform.indexOf(')')).split(','),
-        //     new_height = 1135-;
-        //   sidebar__inner.style.transform = "translate3d(0px, 1135px, 0px)");
-        //   // var translate = $(sidebar).get().css('translate3d');
-        //   // console.log('translate jq=', translate);
-        //   // $(sidebar).css('translate3d', '(0px,1050px,0px)');
-        //   // sidebar.style.translate3d =  '(0px,1050px,0px)';
-        //
-        //
-        //   console.log('"affixed.container-bottom.stickySidebar" triggered');
-        // // }).on('affixed.container-bottom.stickySidebar', function() {
-        // //   console.log('"affixed.container-bottom.stickySidebar" triggered');
-        // });
+            function sidebar_offset() {
+              // return sidebar.offset().top - $('.doc-header-room').outerHeight(true) + parseInt($('.docs-italia-menu').css('padding-top'));
+              // return sidebar.offset().top - parseInt($('.docs-italia-menu').css('padding-top'));
+              // 132 invece di 200
+              // return sidebar.offset().top - $('.doc-header-room').outerHeight(true) - parseInt($('.docs-italia-menu').css('padding-top'));
+              // console.log(sidebar_container.offset().top  + $('header').outerHeight(true));
+              return sidebar_main_container.offset().top  + $('header').outerHeight(true) < scrollPosition;
+            }
+
+            function below_sidebar_bottom(arbitrary_offset) {
+              return scrollPosition + window_height >= sidebar_container_height + scroll_offset + arbitrary_offset;
+            }
+
+            function above_sidebar_top() {
+              return scrollPosition < sidebar.offset().top;
+            }
+
+            function addStickyStyle() {
+              sidebar.addClass('sticky');
+              var width = sidebar.outerWidth(),
+                left = sidebar.offset().left,
+                top = 'auto';
+              sidebar.css('position', 'fixed')
+                .css('width', width)
+                .css('left', left)
+                .css('top', top)
+                .css('transform', '');
+            }
+
+            function addBottomStyle(arbitrary_offset) {
+              console.log('added bottom style');
+              console.log(scrollPosition + window_height, sidebar_container_height + scroll_offset, '=?=', sidebar_main_container.outerHeight() + scroll_offset);
+              addTopStyle();
+              var height = Math.min(
+                (scrollPosition + window_height) - (sidebar_container_height + scroll_offset + arbitrary_offset),
+                (sidebar_main_container.outerHeight() + scroll_offset) - (sidebar_container_height + scroll_offset + arbitrary_offset)
+              );
+              console.log('height', height);
+              sidebar.css('transform',
+                'translateY(' + height +'px)');
+            }
+
+            function addTopStyle() {
+              sidebar.removeClass('sticky');
+              sidebar.css('position', '')
+                .css('width', '')
+                .css('left', '')
+                .css('top', '')
+                .css('transform', '');
+            }
+
+            if (below_sidebar_bottom(20 /* space from the bottom edge of the window */)) {
+              // console.log('you are below');
+              addBottomStyle(20 /* space from the bottom edge of the window */);
+            } else if (above_sidebar_top()) {
+              // console.log('you are above');
+              addTopStyle();
+            }
+            //  else {
+            //   console.log('you are in the middle');
+            //   addStickyStyle();
+            // }
+        });
 
         $('#desktop-menu .collapse').on('shown.bs.collapse', function() {
           // console.log('triggered collapse stuff');
-          stickySidebar.updateSticky();
           $(window).scroll();
         }).on('hidden.bs.collapse', function() {
-          stickySidebar.updateSticky();
+
           $(window).scroll();
           // console.log('triggered collapse stuff');
+        });
+
+        $(document).on('resize', function() {
+          // console.log('triggered collapse stuff');
+          $(window).scroll();
         });
 
       });
